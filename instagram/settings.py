c1b4996291addfa4fed874e39ded4e55e8c233fc
luis1612 <ibarguen1294@gmail.com>
2019-11-24 15:38:52 -0500

    primer deploy

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,18 @@ SECRET_KEY = 'toy^p**jvg*wlh_wc*inq46!*&@x%*dwbuq-r$x&2!+eq^(-g@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd8e7afs0jnfli7',
+        'USER': 'ynfluwzvxbegqn',
+        'PASSWORD': 'b6b22f4d5ccbe4f5c5395b75adc15e7400d716bdc81cf6ca0c9246f315761384',
+        'HOST': 'ec2-107-22-163-220.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Application definition
@@ -84,25 +95,15 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd8e7afs0jnfli7',
-        'USER': 'ynfluwzvxbegqn',
-        'PASSWORD': 'b6b22f4d5ccbe4f5c5395b75adc15e7400d716bdc81cf6ca0c9246f315761384',
-        'HOST': 'ec2-107-22-163-220.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -150,4 +151,4 @@ TEMP = os.path.join(BASE_DIR, 'temp')
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
