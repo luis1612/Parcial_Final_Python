a842d53ff2330a2c29c9ae1ec46e3235e5e62049
luis1612 <ibarguen1294@gmail.com>
2019-11-24 21:19:17 -0500

    deploy

--- instagram/settings.py
+++ instagram/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-from decouple import config,Csv
+from decouple import config
+from unipath import Path
+from dj_database_url import parse as db_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
