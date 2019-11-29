5bdbe857de5c24fe82e745f9971bcf841858c7c5
luis1612 <ibarguen1294@gmail.com>
2019-11-28 20:08:19 -0500

    instalador por consola cmd

---
+++ install.cmd
@@ -0,0 +1,7 @@
+@echo off
+pip install django
+pip install pillow
+python manage.py makemigrations
+python manage.py migrate --fake-initial
+python manage.py createsuperuser
+python manage.py runserver
